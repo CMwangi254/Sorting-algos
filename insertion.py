5e13e36f6e94efcad86b7f983ae88dec853a27bf
malenge254 <techiechic12.34@gmail.com>
2019-06-06 12:32:25 +0000

    'code optimization'

--- insertion.py
+++ insertion.py
@@ -12,16 +12,16 @@ def Rand(start, end, num):
     return res
 
 
-num = 100
+num = 1000
 start = 20
-end = 500
+end = 1500
 a = Rand(start, end, num)
 
 
 def insertion_sort(a):  # define a function for sorting the list
     for i in range(1, len(a)):  # loop through the list starting from the second item
         for j in range(i-1, -1, -1):  # looping through the list tarting from the item adjacent to the subject item
-            if a[j] > a[j+1]:  # if number in subject is greater than the number on the left
+            if a[j] > a[j+1]:  # if number in subject is greater than the number on the right
                 a[j], a[j+1] = a[j+1], a[j]  # swap them
             else:
                 break  # break the loop
@@ -34,3 +34,37 @@ timetaken = time.time()-start_time
 print(timetaken)
 
 
+def insertion_sort(a):  # define a function for sorting the list
+    for i in range(1, len(a)):  # loop through the list starting from the second item
+        j = i-1
+        while a[j] > a[j+1] and j >= 0:
+            a[j], a[j + 1] = a[j + 1], a[j]  # swap them
+            j -= 1
+        else:
+            break
+    return a
+
+
+start_time = time.time()
+print(insertion_sort(a))  # print the list
+timetaken = time.time()-start_time
+print(timetaken)
+
+
+# OPTIMIZED CODE
+def insertion_sort(a):  # define a function for sorting the list
+    for i in range(1, len(a)):  # loop through the list starting from the second item
+        curNum = a[i]
+        for j in range(i - 1, -1, -1):  # looping through the list tarting from the item adjacent to the subject item
+            if a[j] > curNum:
+                a[j+1] = a[j]
+            else:
+                a[j+1] = curNum
+                break
+        return a
+
+
+start_time = time.time()
+print(insertion_sort(a))  # print the list
+timetaken = time.time()-start_time
+print(timetaken)
